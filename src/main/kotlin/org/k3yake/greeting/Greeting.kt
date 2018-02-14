3899ead23cbcf2b56a5412dd0f3e9bb569f1c111
katsuki.miyake <katsuki.miyake@uzabase.com>
2018-02-15 01:28:27 +0900

    [リファクタ]パッケージを移動

--- src/main/kotlin/org/k3yake/greeting/Greeting.kt
+++ src/main/kotlin/org/k3yake/greeting/Greeting.kt
@@ -1,4 +1,4 @@
-package org.k3yake
+package org.k3yake.greeting
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
--- src/main/kotlin/org/k3yake/greeting/GreetingController.kt
+++ src/main/kotlin/org/k3yake/greeting/GreetingController.kt
@@ -1,6 +1,5 @@
-package org.k3yake
+package org.k3yake.greeting
 
-import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
